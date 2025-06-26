b21920757ac523998f84460fc9236f8e20d022be
Brandon Tsai <02.tsaibrandon@gmail.com>
2025-06-25 21:18:42 -0700

    created a basic automation to get to the orders page

---
+++ scraper.py
@@ -0,0 +1,55 @@
+import undetected_chromedriver as uc
+import time
+import json
+import os
+
+from numpy import empty
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = uc.Chrome()
+cookies_file = "cookies.json"
+
+time.sleep(1)
+
+driver.get("https://seller.tcgplayer.com")
+
+time.sleep(2)
+
+if os.path.exists(cookies_file):
+    with open(cookies_file, "r") as f:
+        cookies = json.load(f)
+        for cookie in cookies:
+            if isinstance(cookie.get("expiry"), float):
+                cookie["expiry"] = int(cookie["expiry"])
+            driver.add_cookie(cookie)
+
+    driver.get("https://sellerportal.tcgplayer.com/orders?searchRange=LastThreeMonths&page=1&size=25&sortBy=orderStatus,asc&sortBy=orderDate,asc&qfid=All")
+    input("Check if you were logged in. Press ENTER to continue.")
+
+else:
+    cookie_buttons = driver.find_elements(By.ID, "hs-eu-confirmation-button")
+    if cookie_buttons:
+        ActionChains(driver).move_to_element(cookie_buttons[0]).click().perform()
+
+    login_button = driver.find_element(By.CLASS_NAME, "navbar-interactive-dropdown")
+    ActionChains(driver).move_to_element(login_button).click().perform()
+
+    login_link = driver.find_element(By.CSS_SELECTOR, 'a[href="https://store.tcgplayer.com/admin/account/logon"]')
+    ActionChains(driver).move_to_element(login_link).click().perform()
+
+    time.sleep(2)
+
+    input("Please log in and hit enter")
+
+    with open("cookies.json", "w") as f:
+        json.dump(driver.get_cookies(), f)
+
+    wait = WebDriverWait(driver, 15)
+
+    order_page = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[.//span[text()="Orders"]]')))
+    ActionChains(driver).move_to_element(order_page).click().perform()
